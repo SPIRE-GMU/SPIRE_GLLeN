1f11cbf25532b78c00bdf5f79684b5f1b0f90cbc
SPIRE_GLLeN <mbedewi2@gmu.edu>
2024-10-23 12:23:36 -0400

    basic compile decompiel from exebench

--- LLM4Decompile_content/exebench/justinTest.py
+++ LLM4Decompile_content/exebench/justinTest.py
@@ -1,13 +1,20 @@
 from datasets import load_dataset
 import pprint
 
+
 def main():
-    dataset = load_dataset('jordiae/exebench', split='test_real') # , use_auth_token=True)
+    dataset = load_dataset(
+        "jordiae/exebench", split="test_real"
+    )  # , use_auth_token=True)
     for row in dataset:
-        print('*****')
-        print(row['fname'])  # print function tite
-        print('*****')
-        print(row['func_def'])  # print function definition
-        print('*****')
-        print(row['asm']['code'][0])  # print assembly with the first target, angha_gcc_x86_O0
+        print("*****")
+        print(row["fname"])  # print function tite
+        print("*****")
+        print(row["func_def"])  # print function definition
+        print("*****")
+        print(
+            row["asm"]["code"][0]
+        )  # print assembly with the first target, angha_gcc_x86_O0
+
+
 main()
