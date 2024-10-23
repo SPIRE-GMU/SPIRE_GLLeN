dec24290195ec51d774c9b8f7fbd185ce1b0b739
kali <kali@kali.kali>
2024-10-23 16:08:45 -0400

    try catch added

--- LLM4Decompile_content/exebench/code_tester_5000.py
+++ LLM4Decompile_content/exebench/code_tester_5000.py
@@ -32,40 +32,43 @@ def main():
     # 2) Iterate over dataset
 
     for row in dataset:
-        func0 = row['fname']
-        pprint.pp(row)
-        with open(original_file, "w") as f:
-            f.write(f"#include <stdint.h>\n{row['real_deps']}\n{row['synth_deps']}\nvoid main()\n" + "{}\n" + row['func_def'] + "\n")
-        #    row = 0
-        #    while row < 1:
-        # for row in dataset:
-        # Do stuff with each row
-        # 3) Option A: Manually access row fields. For instance, access the function definition:
-        # pprint.pp(dataset[row])
-        # print('*****')
-        # print(row['func_def'])  # print function definition
-        # print('*****')
-        # print(row['asm']['code'][0])  # print assembly with the first target, angha_gcc_x86_O0
-        # print first I/O example (synthetic)
-        # print('Input:', exebench_dict_to_dict(row['synth_io_pairs']['input'][0]))
-        # print('Output:', exebench_dict_to_dict(row['synth_io_pairs']['output'][0]))
-        # print(row['synth_exe_wrapper'][0])  # print C++ wrapper to run function with IO
-        # You can manually compile, run with IO, etc
-        #       row += 1
+        try:
+            func0 = row['fname']
+            # pprint.pp(row)
+            with open(original_file, "w") as f:
+                f.write(f"#include <stdint.h>\n{row['real_deps']}\n{row['synth_deps']}\nvoid main()\n" + "{}\n" + row['func_def'] + "\n")
+            #    row = 0
+            #    while row < 1:
+            # for row in dataset:
+            # Do stuff with each row
+            # 3) Option A: Manually access row fields. For instance, access the function definition:
+            # pprint.pp(dataset[row])
+            # print('*****')
+            # print(row['func_def'])  # print function definition
+            # print('*****')
+            # print(row['asm']['code'][0])  # print assembly with the first target, angha_gcc_x86_O0
+            # print first I/O example (synthetic)
+            # print('Input:', exebench_dict_to_dict(row['synth_io_pairs']['input'][0]))
+            # print('Output:', exebench_dict_to_dict(row['synth_io_pairs']['output'][0]))
+            # print(row['synth_exe_wrapper'][0])  # print C++ wrapper to run function with IO
+            # You can manually compile, run with IO, etc
+            #       row += 1
 
 
 
-        asm_file = assemble(original_file, origin_no_path, func0)
+            asm_file = assemble(original_file, origin_no_path, func0)
 
-        decompiler(asm_file, func0, recompiled_file)
+            decompiler(asm_file, func0, recompiled_file)
 
-            # Clear the terminal screen
+                # Clear the terminal screen
 
-            # Display the ASCII art
-        
-            # Get a list of files with .c or .o extensions in the current working directory
+                # Display the ASCII art
+            
+                # Get a list of files with .c or .o extensions in the current working directory
 
-        break
+            break
+        except:
+            pass
     # TODO recompiled_file
 
     return 0
