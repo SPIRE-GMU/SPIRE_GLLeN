833fd18e401fd7d027d2687eb9d190fe1515b5a9
kali <kali@kali.kali>
2024-10-23 15:38:08 -0400

    first half of code tester 5000 done

--- LLM4Decompile_content/exebench/code_tester_5000.py
+++ LLM4Decompile_content/exebench/code_tester_5000.py
@@ -14,8 +14,11 @@ import argparse
 
 MODEL_PATH = "/home/spire2/LLM4Decompile/llm4decompile-22b-v2"
 
-func0 = ""
-
+original_file = 'origin.c'
+origin_no_path = 'origin'
+recompiled_file = 'new_file.c'
+recompile_no_path = 'new_file'
+garbage_file_count = 0
 
 def main():
     """
@@ -26,118 +29,145 @@ def main():
         "jordiae/exebench", split="test_synth"
     )  # , use_auth_token=True)
     # 2) Iterate over dataset
-    print(len(dataset))
-    #    row = 0
-    #    while row < 1:
-    # for row in dataset:
-    # Do stuff with each row
-    # 3) Option A: Manually access row fields. For instance, access the function definition:
-    # pprint.pp(dataset[row])
-    # print('*****')
-    # print(row['func_def'])  # print function definition
-    # print('*****')
-    # print(row['asm']['code'][0])  # print assembly with the first target, angha_gcc_x86_O0
-    # print first I/O example (synthetic)
-    # print('Input:', exebench_dict_to_dict(row['synth_io_pairs']['input'][0]))
-    # print('Output:', exebench_dict_to_dict(row['synth_io_pairs']['output'][0]))
-    # print(row['synth_exe_wrapper'][0])  # print C++ wrapper to run function with IO
-    # You can manually compile, run with IO, etc
-    #       row += 1
 
-    parser = argparse.ArgumentParser(description="Decompile assembly code")
-    parser.add_argument("-c", "--compile", action="store_true", help="Compile the code")
-    parser.add_argument(
-        "-d", "--decompile", action="store_true", help="Decompile the assembly code"
+    for row in dataset:
+        func0 = row['fname']
+        with open(original_file, "w") as f:
+            f.write("void main()\n{}\n" + row['func_def'])
+        #    row = 0
+        #    while row < 1:
+        # for row in dataset:
+        # Do stuff with each row
+        # 3) Option A: Manually access row fields. For instance, access the function definition:
+        # pprint.pp(dataset[row])
+        # print('*****')
+        # print(row['func_def'])  # print function definition
+        # print('*****')
+        # print(row['asm']['code'][0])  # print assembly with the first target, angha_gcc_x86_O0
+        # print first I/O example (synthetic)
+        # print('Input:', exebench_dict_to_dict(row['synth_io_pairs']['input'][0]))
+        # print('Output:', exebench_dict_to_dict(row['synth_io_pairs']['output'][0]))
+        # print(row['synth_exe_wrapper'][0])  # print C++ wrapper to run function with IO
+        # You can manually compile, run with IO, etc
+        #       row += 1
+
+
+
+        asm_file = assemble(original_file, origin_no_path, func0)
+
+        decompiler(asm_file, func0, recompiled_file)
+
+            # Clear the terminal screen
+
+            # Display the ASCII art
+        
+            # Get a list of files with .c or .o extensions in the current working directory
+
+        break
+    # TODO recompiled_file
+
+    return 0
+
+
+def assemble(name, name_no_path, function_name):
+    asm_file_name = f"{name_no_path}.asm"
+    s_file_name = f"{name_no_path}.s"
+    obj_file_name = f"{name_no_path}.o"
+    # os.system(f"gcc -c {file_name} -o {obj_file_name} && objdump -d {obj_file_name} > {asm_file_name}")
+    # os.system(f"gcc -c {file_name} -o {obj_file_name}")
+
+    compile_command = (
+        f"gcc -o {obj_file_name} {name} -lm"  # compile the code with GCC on Linux
     )
-    parser.add_argument("-f", "--file", type=str, help="Input file path")
-    parser.add_argument("-t", "--function", type=str, help="Function name to decompile")
-    parser.add_argument("-m", "--model", type=str, help="Model size (6.7b, 9b, 22b)")
-    args = parser.parse_args()
+    subprocess.run(compile_command, shell=True, check=True)
+    compile_command = f"objdump -d {obj_file_name}> {s_file_name}"  # disassemble the binary file into assembly instructions
+    subprocess.run(compile_command, shell=True, check=True)
 
-    if args.compile:
-        assemble(args.file, args.model, args.function)
-    elif args.decompile:
-        if args.file and args.function and args.model:
-            if args.model not in MODEL_PATHS:
-                raise ValueError(
-                    "Invalid model option. Choose from '6.7b', '9b', or '22b'."
-                )
-            run(args.file, args.model, args.function)
-        else:
-            print("Error: Please provide the file name, function name, and model size.")
-    else:
-        # Clear the terminal screen
-        os.system("clear")
+    input_asm = ""
+    with open(s_file_name) as f:  # asm file
+        asm = f.read()
+        if (
+            "<" + function_name + ">:" not in asm
+        ):  # IMPORTANT replace func0 with the function name
+            raise ValueError("compile fails")
+        asm = (
+            "<"
+            + function_name
+            + ">:"
+            + asm.split("<" + function_name + ">:")[-1].split("\n\n")[0]
+        )  # IMPORTANT replace func0 with the function name
+        asm_clean = ""
+        asm_sp = asm.split("\n")
+        for tmp in asm_sp:
+            if len(tmp.split("\t")) < 3 and "00" in tmp:
+                continue
+            idx = min(len(tmp.split("\t")) - 1, 2)
+            tmp_asm = "\t".join(tmp.split("\t")[idx:])  # remove the binary code
+            tmp_asm = tmp_asm.split("#")[0].strip()  # remove the comments
+            asm_clean += tmp_asm + "\n"
+    input_asm = asm_clean.strip()
+    before = f"# This is the assembly code:\n"  # prompt
+    after = "\n# What is the source code?\n"  # prompt
+    input_asm_prompt = before + input_asm.strip() + after
+    with open(asm_file_name, "w", encoding="utf-8") as f:
+        f.write(input_asm_prompt)
 
-        # Display the ASCII art
-        print(TITLE_CARD)
+    print(f"Assembly file generated: {asm_file_name}")
 
-        # Get a list of files with .c or .o extensions in the current working directory
-        current_dir = os.path.dirname(__file__)
-        files = [
-            f for f in os.listdir(current_dir) if f.endswith(".c") or f.endswith(".asm")
-        ]
-
-        # Prompt the user for the file name and display the list of files
-        print("Select the file name of the file:")
-        # print("Available files:")
-        for i, file in enumerate(files, start=1):
-            print(f"{i}. {file}")
-
-        # Get user input as a string
-        file_input = input()
-
-        # Check if the input is a digit
-        if file_input.isdigit():
-            file_number = int(file_input)
-            if file_number < 1 or file_number > len(files):
-                raise ValueError("Invalid file number.")
-            file_name = files[file_number - 1]
-        else:
-            # Check if the input is a file name
-            if file_input in files:
-                file_name = file_input
-            else:
-                raise ValueError("Invalid file name.")
-
-        if file_name.endswith(".c"):
-
-            # Get user input for the function name
-            print("Enter the function name to decompile: [main]")
-            func0 = input()
-
-            if assemble(file_name, func0) == 0:
-                print("Compilation successful.")
-
-            return 0
-
-        else:
-            # Get user input for the model size
-
-            # Get user input for the function name
-            print("Enter the function name to decompile: [main]")
-            func0 = input()
-
-            # Run the decompilation process
-            return run(file_name, model_size, func0)
-
-    # TODO
-
-    return 0
-
-
-def assemble():
-    # TODO
-    return 0
+    return asm_file_name
 
 
 def load_model():
-    # TODO
-    return 0
+    model_path = MODEL_PATH
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_path,
+        torch_dtype=torch.bfloat16,
+        low_cpu_mem_usage=True,  # Helps with large models
+        device_map="auto",  # Automatically maps layers to available devices
+        trust_remote_code=True,  # Trusts remote code, necessary for FlashAttention
+    )
+    return tokenizer, model
 
 
-def decompiler():
-    # TODO
+def decompiler(old_file_name, func_name, new_file_name):
+    accelerator = Accelerator()
+
+    # Load the tokenizer and model
+    tokenizer, model = load_model()
+    model = accelerator.prepare(model)
+
+    # Read the assembly function
+    # asm_file_path = f"{file_name}_{OPT_LEVELS[0]}.asm"
+    # with open(asm_file_path, 'r') as f:
+
+    # read the assembly function
+    with open(old_file_name, "r") as f:
+        asm_function = f.read()
+
+    # Allocate the inputs tensor on the accelerator device
+    inputs = tokenizer(asm_function, return_tensors="pt").to(accelerator.device)
+
+    with torch.no_grad():
+        # Use FlashAttention during generation
+        outputs = model.generate(
+            **inputs, max_new_tokens=2048
+        )  # Max length to 4096, max new tokens should be below the range
+
+    # Decode the generated output
+    decompiled_function = tokenizer.decode(outputs[0][len(inputs["input_ids"][0]) : -1])
+
+    # Read the original C file
+    # with open(file_name + '.c', 'r') as f:
+    # original_function = f.read()
+
+    # Print the original and decompiled functions
+    # print(f"Original function:\n{original_function}")
+
+    # Ask the user if they want to save the decompiled function to a file
+
+    with open(new_file_name, "w") as f:
+        f.write(decompiled_function)
     return 0
 
 
