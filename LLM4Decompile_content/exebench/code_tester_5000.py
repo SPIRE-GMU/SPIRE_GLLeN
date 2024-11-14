290ffa5f39e68c765501c2be2f8ab0b4bdba5805
kali <kali@kali.kali>
2024-11-14 17:23:19 -0500

    optimization additions

--- LLM4Decompile_content/exebench/code_tester_5000.py
+++ LLM4Decompile_content/exebench/code_tester_5000.py
@@ -18,19 +18,25 @@ import argparse
 
 MODEL_PATH = "/home/spire2/LLM4Decompile/llm4decompile-22b-v2"
 
-original_file = 'origin.c'
-origin_no_path = 'origin'
-recompiled_file = 'new_file.c'
-recompile_no_path = 'new_file'
 
+origin_no_path = 'origin'
+
+recompile_no_path = 'new_file'
+optimization = ["O1", "O2", "O3"]
 
 def main():
     """
     The main function controling all other functions
     """
     currently_a_success = True
-    garbage_file_count = 0
-    success_count = 0
+    garbage_file_count_1 = 0
+    garbage_file_count_2 = 0
+    garbage_file_count_3 = 0
+
+    success_count_1 = 0
+    success_count_2 = 0
+    success_count_3 = 0
+
     # 1) Load dataset split. In this case, synthetic test split
     dataset = load_dataset(
         "jordiae/exebench", split="train_real_compilable"
@@ -38,69 +44,97 @@ def main():
     # 2) Iterate over dataset
     tokenizer, model = load_model()
     for row in dataset:
-        try:
-            func0 = row['fname']
-            # pprint.pp(row)
-            with open(original_file, "w") as f:
-                f.write("#include <stdint.h>\n#include <stdio.h>\n" + row['real_deps'] + '\n' + row['func_def'])
-            f.close()
-            #    row = 0
-            #    while row < 1:
-            # for row in dataset:
-            # Do stuff with each row
-            # 3) Option A: Manually access row fields. For instance, access the function definition:
-            # pprint.pp(dataset[row])
-            # print('*****')
-            # print(row['func_def'])  # print function definition
-            # print('*****')
-            # print(row['asm']['code'][0])  # print assembly with the first target, angha_gcc_x86_O0
-            # print first I/O example (synthetic)
-            # print('Input:', exebench_dict_to_dict(row['synth_io_pairs']['input'][0]))
-            # print('Output:', exebench_dict_to_dict(row['synth_io_pairs']['output'][0]))
-            # print(row['synth_exe_wrapper'][0])  # print C++ wrapper to run function with IO
-            # You can manually compile, run with IO, etc
-            #       row += 1
+        for opt in range(len(optimization)):
+            recompiled_file = 'new_file' + optimization[opt] + '.c'
+            original_file = 'origin' + optimization[opt] + '.c'
+            try:
+                func0 = row['fname']
+                # pprint.pp(row)
+                with open(original_file, "w") as f:
+                    f.write("#include <stdint.h>\n#include <stdio.h>\n" + row['real_deps'] + '\n' + row['func_def'])
+                f.close()
+                #    row = 0
+                #    while row < 1:
+                # for row in dataset:
+                # Do stuff with each row
+                # 3) Option A: Manually access row fields. For instance, access the function definition:
+                # pprint.pp(dataset[row])
+                # print('*****')
+                # print(row['func_def'])  # print function definition
+                # print('*****')
+                # print(row['asm']['code'][0])  # print assembly with the first target, angha_gcc_x86_O0
+                # print first I/O example (synthetic)
+                # print('Input:', exebench_dict_to_dict(row['synth_io_pairs']['input'][0]))
+                # print('Output:', exebench_dict_to_dict(row['synth_io_pairs']['output'][0]))
+                # print(row['synth_exe_wrapper'][0])  # print C++ wrapper to run function with IO
+                # You can manually compile, run with IO, etc
+                #       row += 1
 
 
+                
+                asm_file = assemble(original_file, origin_no_path, func0, optimization[opt])
 
-            asm_file = assemble(original_file, origin_no_path, func0)
+                decompiled_func = decompiler(asm_file, tokenizer, model)
 
-            decompiled_func = decompiler(asm_file, tokenizer, model)
+                with open(recompiled_file, "w") as d:
+                    d.write("#include <stdint.h>\n#include <stdio.h>\n" + row['real_deps'] + '\n' + decompiled_func)
+                d.close()
 
-            with open(recompiled_file, "w") as d:
-                d.write("#include <stdint.h>\n#include <stdio.h>\n" + row['real_deps'] + '\n' + decompiled_func)
-            d.close()
+                currently_a_success = True
 
-            currently_a_success = True
+                    # Clear the terminal screen
 
-                # Clear the terminal screen
+                    # Display the ASCII art
+                
+                    # Get a list of files with .c or .o extensions in the current working directory
 
-                # Display the ASCII art
-            
-                # Get a list of files with .c or .o extensions in the current working directory
+            except:
+                print('This file sucks becuse exebench sucks')
+                currently_a_success = False
+            try:
+                if currently_a_success:
+                    if opt == 1:
+                        assemble(recompiled_file, recompile_no_path, func0, optimization[opt])
+                        shutil.copy(original_file, f'red-team-success/{origin_no_path}{optimization[opt]}{success_count_1}.c')
+                        shutil.copy(recompiled_file, f'red-team-success/{recompile_no_path}{optimization[opt]}{success_count_1}.c')
 
-        except:
-            print('This file sucks becuse exebench sucks')
-            currently_a_success = False
-        try:
-            if currently_a_success:
-                assemble(recompiled_file, recompile_no_path, func0)
-                shutil.copy(original_file, f'red-team-success/{origin_no_path}{success_count}.c')
-                shutil.copy(recompiled_file, f'red-team-success/{recompile_no_path}{success_count}.c')
-                success_count += 1
-            else: 
-                pass
-        except:
-            shutil.copy(original_file, f'red-team-failures/{origin_no_path}{garbage_file_count}.c')
-            shutil.copy(recompiled_file, f'red-team-failures/{recompile_no_path}{garbage_file_count}.c')
-            garbage_file_count += 1
+                        success_count_1 += 1
+                    elif opt == 2:
+                        assemble(recompiled_file, recompile_no_path, func0, optimization[opt])
+                        shutil.copy(original_file, f'red-team-success/{origin_no_path}{optimization[opt]}{success_count_2}.c')
+                        shutil.copy(recompiled_file, f'red-team-success/{recompile_no_path}{optimization[opt]}{success_count_2}.c')
+                        success_count_2 += 1
+                    elif opt == 3:
+                        assemble(recompiled_file, recompile_no_path, func0, optimization[opt])
+                        shutil.copy(original_file, f'red-team-success/{origin_no_path}{optimization[opt]}{success_count_3}.c')
+                        shutil.copy(recompiled_file, f'red-team-success/{recompile_no_path}{optimization[opt]}{success_count_3}.c')
+                        success_count_3 += 1
+
+                    else: 
+                        pass
+            except:
+                if opt == 1:
+
+                    shutil.copy(original_file, f'red-team-failures/{origin_no_path}{optimization[opt]}{garbage_file_count_1}.c')
+                    shutil.copy(recompiled_file, f'red-team-failures/{recompile_no_path}{optimization[opt]}{garbage_file_count_1}.c')
+                    garbage_file_count_1 += 1
+                elif opt == 2:
+
+                    shutil.copy(original_file, f'red-team-failures/{origin_no_path}{optimization[opt]}{garbage_file_count_2}.c')
+                    shutil.copy(recompiled_file, f'red-team-failures/{recompile_no_path}{optimization[opt]}{garbage_file_count_2}.c') 
+                    garbage_file_count_2 += 1 
+                elif opt == 3:
+
+                    shutil.copy(original_file, f'red-team-failures/{origin_no_path}{optimization[opt]}{garbage_file_count_3}.c')
+                    shutil.copy(recompiled_file, f'red-team-failures/{recompile_no_path}{optimization[opt]}{garbage_file_count_3}.c')  
+                    garbage_file_count_3 += 1           
 
     # TODO recompiled_file
 
     return 0
 
 
-def assemble(name, name_no_path, function_name):
+def assemble(name, name_no_path, function_name, optimization):
     asm_file_name = f"{name_no_path}.asm"
     s_file_name = f"{name_no_path}.s"
     obj_file_name = f"{name_no_path}.o"
@@ -108,7 +142,7 @@ def assemble(name, name_no_path, function_name):
     # os.system(f"gcc -c {file_name} -o {obj_file_name}")
 
     compile_command = (
-        f"gcc -c {name} -o {obj_file_name} -lm"  # compile the code with GCC on Linux
+        f"gcc -c {name} -o {obj_file_name} -{optimization} -lm"  # compile the code with GCC on Linux
     )
     subprocess.run(compile_command, shell=True, check=True)
     compile_command = f"objdump -d {obj_file_name}> {s_file_name}"  # disassemble the binary file into assembly instructions
