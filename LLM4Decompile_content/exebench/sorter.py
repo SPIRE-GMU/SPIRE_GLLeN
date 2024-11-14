8287fe4f28b5fdb939e6b0dd2bddc5e854a27a2c
kali <kali@kali.kali>
2024-11-14 14:18:11 -0500

    mocing again

--- LLM4Decompile_content/exebench/sorter.py
+++ LLM4Decompile_content/exebench/sorter.py
@@ -14,15 +14,17 @@ while file_num < 10:
     
     try:
         test = subprocess.run(compile_command, capture_output=True, text=True)
-        print(test.stderr)
+        if "expected declaration or statement" in test.stderr:
+            print(test.stderr)
         file_num +=1
-    except subprocess.CalledProcessError as exc:
-        print(exc.stderr, exc.stdout)
-        file_num += 1
         name = "new_file" + str(file_num) + ".c"
         compile_command = [
             "gcc", '-c', name, '-o', 'testing', "-lm"  # compile the code with GCC on Linux
         ]
+    except subprocess.CalledProcessError as exc:
+        print(exc.stderr, exc.stdout)
+        file_num += 1
+
 
 
 
