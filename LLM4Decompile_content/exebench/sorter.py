e8bf29735779c750a2f541bef815d0d232bb678c
kali <kali@kali.kali>
2024-11-14 14:22:32 -0500

    mocing again

--- LLM4Decompile_content/exebench/sorter.py
+++ LLM4Decompile_content/exebench/sorter.py
@@ -11,7 +11,7 @@ import_errors = 0
 compile_command = [
         "gcc", '-c', name, '-o', 'testing', "-lm"  # compile the code with GCC on Linux
 ]
-while file_num < 10:
+while file_num < 100:
     
     try:
         test = subprocess.run(compile_command, capture_output=True, text=True)
