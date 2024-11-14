85285c1eff766f6ed5bbf9ab92333e2da6429a1b
kali <kali@kali.kali>
2024-11-14 14:06:49 -0500

    mocing again

--- LLM4Decompile_content/exebench/sorter.py
+++ LLM4Decompile_content/exebench/sorter.py
@@ -12,14 +12,16 @@ compile_command = [
 ]
 while file_num < 10:
     
-
-    test = subprocess.Popen(compile_command, stderr=subprocess.PIPE)
-    print(test[5])
-    file_num += 1
-    name = "new_file" + str(file_num) + ".c"
-    compile_command = [
-        "gcc", '-c', name, '-o', 'testing', "-lm"  # compile the code with GCC on Linux
-    ]
+    try:
+        test = subprocess.check_output(compile_command, stderr=subprocess.STDOUT, shell=True, timeout=3,
+        universal_newlines=True)
+    except subprocess.CalledProcessError as exc:
+        print(exec.output)
+        file_num += 1
+        name = "new_file" + str(file_num) + ".c"
+        compile_command = [
+            "gcc", '-c', name, '-o', 'testing', "-lm"  # compile the code with GCC on Linux
+        ]
 
 
 
