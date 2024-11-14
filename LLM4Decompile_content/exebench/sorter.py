fc814d75f87320132293c989242e56b5b45a743a
kali <kali@kali.kali>
2024-11-14 14:22:13 -0500

    mocing again

--- LLM4Decompile_content/exebench/sorter.py
+++ LLM4Decompile_content/exebench/sorter.py
@@ -6,7 +6,8 @@ import subprocess
 
 file_num = 0
 name = "new_file" + str(file_num) + ".c"
-
+jump_errors = 0
+import_errors = 0
 compile_command = [
         "gcc", '-c', name, '-o', 'testing', "-lm"  # compile the code with GCC on Linux
 ]
@@ -15,17 +16,17 @@ while file_num < 10:
     try:
         test = subprocess.run(compile_command, capture_output=True, text=True)
         if "expected declaration or statement" in test.stderr:
+            jump_errors += 1
+        elif "expected identifier or '(' at end of input" in test.stderr:
             print(test.stderr)
-        else:
-            print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+
         file_num +=1
         name = "new_file" + str(file_num) + ".c"
         compile_command = [
             "gcc", '-c', name, '-o', 'testing', "-lm"  # compile the code with GCC on Linux
         ]
-    except subprocess.CalledProcessError as exc:
-        print(exc.stderr, exc.stdout)
-        file_num += 1
+    except:
+        break
 
 
 
