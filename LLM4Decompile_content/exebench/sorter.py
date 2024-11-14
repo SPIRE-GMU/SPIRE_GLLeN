19df48da9a5ee3ca1329af7ac9a65b7b1f137639
kali <kali@kali.kali>
2024-11-14 13:57:53 -0500

    mocing again

--- LLM4Decompile_content/exebench/sorter.py
+++ LLM4Decompile_content/exebench/sorter.py
@@ -14,8 +14,9 @@ while file_num < 10:
     
     try:
         test = subprocess.check_output(compile_command)
-        print(test)
+        
     except:
+        print(test)
         file_num += 1
         name = "new_file" + str(file_num) + ".c"
         compile_command = [
